6cd622904cc11b8de98b2be90b300b0ee2c16f02
Romain QUEY <quey@emse.fr>
2021-06-11 14:26:41 +0200

    Update website to version 4.2.0

--- imgs/gene_peri.sh
+++ imgs/gene_peri.sh
@@ -2,11 +2,11 @@
 
 NEPER="neper --rcfile none"
 
-$NEPER -T -n 1000 -morpho gg -per 1   -o gene_peri_1
-$NEPER -T -n 1000 -morpho gg -per x   -o gene_peri_2
-$NEPER -T -n 1000 -morpho gg -per x,y -o gene_peri_3
+# $NEPER -T -n 1000 -morpho gg -per 1   -o gene_peri_1
+# $NEPER -T -n 1000 -morpho gg -per x   -o gene_peri_2
+# $NEPER -T -n 1000 -morpho gg -per x,y -o gene_peri_3
 
-C="-datacellcol id -datacelltrs 0.5 -cameraangle 12 -cameracoo x:-6.3:z -cameraprojection orthographic"
+C="-datacellcol id -dataedgerad 0.002 -datacelltrs 0.5 -cameraangle 14 -cameracoo x:-6.3:z -cameraprojection orthographic"
 for i in 1 2 3
 do
   $NEPER -V gene_peri_$i.tess $C -print gene_peri_$i
